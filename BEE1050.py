1dd3214d70f4b5ee503ef751e14c28299f8dec73
MauroSon <mcontaalternativa@gmail.com>
2023-08-17 21:59:24 -0300

    BEE1050

---
+++ BEE1050.py
@@ -0,0 +1,13 @@
+dicionario = {
+    61:'Brasilia',
+    71:'Salvador',
+    11:'Sao Paulo',
+    21:'Rio de Janeiro',
+    32:'Juiz de Fora',
+    19:'Campinas',
+    27:'Vitoria',
+    31:'Belo Horizonte'
+    }
+data_input=input()
+if data_input in dicionario:
+    print(data_input)

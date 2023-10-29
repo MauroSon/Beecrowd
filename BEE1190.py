0fed5ab44079784bf3dc1cbc11fb76b5b0a992bc
MauroSon <127454440+MauroSon@users.noreply.github.com>
2023-10-29 16:49:10 -0300

    Create BEE1190.py

---
+++ BEE1190.py
@@ -0,0 +1,29 @@
+operacao = input()
+k=10
+i=0
+matriz=[]
+soma = 0
+cont=0
+while i<12:
+    linha = []
+    l=0
+    while l<12:
+        linha.append(float(input()))
+        l+=1
+    matriz.append(linha)
+    i+=1
+for elem in range(1,12):
+    if elem<6:
+        for x in range(11,k,-1):
+            soma+=matriz[elem][x]
+            cont+=1
+        k-=1
+    else:
+        k+=1
+        for y in range(11,k,-1):
+            soma+=matriz[elem][y]
+            cont+=1
+if operacao=='S':
+    print(f'{soma:.1f}')
+else:
+    print(f'{soma/cont:.1f}')
